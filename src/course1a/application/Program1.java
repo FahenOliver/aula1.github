7a7e9ae1e47d46e9a4ea10e02ec30099d6e3de6c
FahenOliver <fahen.oliver@gmail.com>
2019-10-10 22:54:56 -0300

    Depois de apagar o projeto e importar de volta, modificação de teste foi feita para commitar e fazer testes

--- src/course1a/application/Program1.java
+++ src/course1a/application/Program1.java
@@ -1,5 +1,9 @@
 package course1a.application;
 
-public class Program1 {
+import javax.swing.JOptionPane;
 
-}
+public class Program1 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olá mundo!!!", "Primeiro Olá no git",JOptionPane.INFORMATION_MESSAGE);
+	}//fim de main
+}//fim de classe
